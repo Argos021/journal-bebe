0428d14e5de65a08811aa13e9160f7263a455a12
Argos021 <32138921+Argos021@users.noreply.github.com>
2026-04-24 21:18:59 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
-
+import { getAuth } from "firebase/auth";
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyCC54iIWV3mopeoz0Lvaq4aM7lEweQQjvc",
   authDomain: "journal-bebe.firebaseapp.com",
@@ -10,10 +11,11 @@ const firebaseConfig = {
   appId: "1:599551246218:web:8a748c479aa9f2f2fc5273",
   measurementId: "G-S2DJFNE4EV"
 };
-
+ 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
+export const auth = getAuth(app);
+ 
 // Enable offline persistence so the app works without internet
 enableIndexedDbPersistence(db).catch((err) => {
   if (err.code === 'failed-precondition') {
